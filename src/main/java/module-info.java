4026dc33063460e5f9c39dba7884ae84d7cf8fa4
JisusSestiveDoff <86656641+JisusSestiveDoff@users.noreply.github.com>
2022-04-29 12:25:17 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.curves {
     requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
+    requires transitive javafx.fxml;
+    requires transitive javafx.graphics;
+    requires transitive java.sql;
     requires java.desktop;
     opens com to javafx.fxml;
     exports com;

f933afb0229a2dce5387c45b2eb3a9917bdedb6f
JisusSestiveDoff <86656641+JisusSestiveDoff@users.noreply.github.com>
2022-05-16 10:16:34 +0300

    Update App.java

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,20 +1,26 @@
 package com;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import javafx.scene.layout.AnchorPane;
+
 /**
  * JavaFX App
  */
 public class App extends Application {
-    static Stage stage;  
+    static Stage stage;
     private static Scene scene;
     static FXMLLoader fxmlLoader;
 
@@ -25,21 +31,38 @@ public class App extends Application {
 
         stage.setScene(scene);
         stage.show();
+        stage.getScene().getWindow().addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, new EventHandler<WindowEvent>() {
+
+            @Override
+            public void handle(WindowEvent event) {
+                Alert alert = new Alert(AlertType.CONFIRMATION, "Do you want to save the changes?",
+                        ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+                alert.setTitle("Save before closing");
+                alert.showAndWait();
+                if (alert.getResult() == ButtonType.YES) {
+                    MainPage.saveProcess();
+                } else  if (alert.getResult() == ButtonType.CANCEL) {
+                    event.consume();
+                }
+            }
+
+        });
         stage.setTitle("SQLite GUI");
         MainPage.showStartTipLabel();
-        AnchorPane.setRightAnchor((HBox)scene.lookup("#topBox"),10.0);
+        AnchorPane.setRightAnchor((HBox) scene.lookup("#topBox"), 10.0);
         System.out.println();
         MainPage.FilePicker(fxmlLoader);
-        
+        MainPage.SaveAction(fxmlLoader);
 
     }
 
-    static Scene getScene(){
+    static Scene getScene() {
         return scene;
-    } 
-    static Stage getStage(){
+    }
+
+    static Stage getStage() {
         return stage;
-    } 
+    }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
@@ -55,4 +78,8 @@ public class App extends Application {
         launch();
     }
 
+    @Override
+    public void stop() {
+
+    }
 }

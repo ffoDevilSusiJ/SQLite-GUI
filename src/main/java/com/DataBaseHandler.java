2cb94e5e5c98c41b28afbdee5ca07686dd8d11dd
JisusSestiveDoff <86656641+JisusSestiveDoff@users.noreply.github.com>
2022-05-23 23:19:59 +0300

    Update DataBaseHandler.java

--- src/main/java/com/DataBaseHandler.java
+++ src/main/java/com/DataBaseHandler.java
@@ -1,88 +1,102 @@
 package com;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataBaseHandler {
 
-    public static Connection dbConnection;
+	public static Connection dbConnection;
 
-    protected static Connection getDBConnection(String connectionString ) throws ClassNotFoundException, SQLException {
+	protected static Connection getDBConnection(String connectionString) throws ClassNotFoundException, SQLException {
 		Class.forName("org.sqlite.JDBC");
 		Connection connection = DriverManager.getConnection(connectionString);
 		return connection;
 	}
-    
+
 	public static void openSQLFile(String nameFile) throws ClassNotFoundException, SQLException {
-		if(nameFile != null) { 
+		if (nameFile != null) {
 			try {
 				String connection = "jdbc:sqlite:" + nameFile;
 				dbConnection = getDBConnection(connection);
-			} catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println("File not found");
 			}
-		} else System.out.println("File not found");
-        System.out.println("File found");
+		} else
+			System.out.println("File not found");
+		System.out.println("File found");
 	}
 
-    public static ResultSet getTablesSet() throws SQLException, ClassNotFoundException {
+	public static ResultSet getTablesSet() throws SQLException, ClassNotFoundException {
 		java.sql.Statement statement = dbConnection.createStatement();
-        
+
 		String query = "SELECT name FROM sqlite_master WHERE type='table';";
 		ResultSet rs = null;
-			
-			rs = statement.executeQuery(query);
-		//statement.close();
+
+		rs = statement.executeQuery(query);
+		// statement.close();
 		return rs;
 	}
 
-    public static ResultSet getTableItem(String item_type) throws SQLException, ClassNotFoundException {
+	public static ResultSet getTableItem(String item_type) throws SQLException, ClassNotFoundException {
 		java.sql.Statement statement = dbConnection.createStatement();
-        
+
 		String query = "SELECT * FROM " + item_type + ";";
 		ResultSet rs = null;
-			
-			rs = statement.executeQuery(query);
-		//statement.close();
+		rs = statement.executeQuery(query);
+		// statement.close();
 		return rs;
 	}
 
-	public static void updateValue(String table, String field, int rowid, String value) throws SQLException{
-		String query = "UPDATE "+ table  +" SET " + field + " = ?" + " Where " + "rowid" + " = ?" + ";";
-		System.out.println(table +" "  + field + " " + rowid);
+	public static void updateValue(String table, String field, int rowid, String value) throws SQLException {
+		String query = "UPDATE " + table + " SET " + field + " = ?" + " Where " + "rowid" + " = ?" + ";";
+		System.out.println(table + " " + field + " " + rowid);
 		PreparedStatement statement = dbConnection.prepareStatement(query);
 		statement.setString(1, value);
 		statement.setInt(2, rowid);
 		statement.executeUpdate();
 	}
 
-	public static void addItem(String table, List<String> fields) throws SQLException, ClassNotFoundException{
+	public static ResultSet getPrimaryKeys(String table) throws SQLException {
+		DatabaseMetaData meta = dbConnection.getMetaData();
+		ResultSet primaryKeys = null;
+		ResultSet tables = meta.getTables(null, null, "%", new String[] { "TABLE" });
+		String catalog = tables.getString("TABLE_CAT");
+		String schema = tables.getString("TABLE_SCHEM");
+		String tableName = table;
+		primaryKeys = meta.getPrimaryKeys(catalog, schema, tableName);
+		return primaryKeys;
+	}
+
+	public static void addItem(String table, List<String> fields) throws SQLException, ClassNotFoundException {
 		ResultSet rs = getTableItem(table);
 		String query = "INSERT INTO " + table + " VALUES ( ";
 		for (int i = 0; i < fields.size(); i++) {
-			if(i != fields.size() - 1)
-			query = query + "?" + ",";
-			else query = query + "?";
+			if (i != fields.size() - 1)
+				query = query + "?" + ",";
+			else
+				query = query + "?";
 		}
 		query = query + ");";
 		System.out.println(query);
 		PreparedStatement statement = dbConnection.prepareStatement(query);
 		System.out.println();
 		for (int i = 0; i < fields.size(); i++) {
-			if(rs.getMetaData().getColumnType((i + 1)) == java.sql.Types.INTEGER){
+			if (rs.getMetaData().getColumnType((i + 1)) == java.sql.Types.INTEGER) {
 				statement.setInt((i + 1), Integer.valueOf(fields.get(i)));
 			}
-			if(rs.getMetaData().getColumnType((i + 1)) == java.sql.Types.VARCHAR){
+			if (rs.getMetaData().getColumnType((i + 1)) == java.sql.Types.VARCHAR) {
 				statement.setString((i + 1), fields.get(i));
 			}
 		}
-		
+
 		statement.executeUpdate();
 	}
 }

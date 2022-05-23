6f7052b7baf54df7e684645eb65d2067485ae4af
JisusSestiveDoff <86656641+JisusSestiveDoff@users.noreply.github.com>
2022-05-23 22:08:09 +0300

    Update Cleaner.java

--- src/main/java/com/Cleaner.java
+++ src/main/java/com/Cleaner.java
@@ -7,24 +7,30 @@ import javafx.scene.layout.VBox;
 
 public class Cleaner {
 
-    static Scene scene = App.getScene();
+    static Scene mainPage = App.getScene();
+    static Scene addItemDialog = AddItemDialog.getScene();
 
     public static void newItemList() {
-        VBox gridContainer = (VBox) scene.lookup("#gridContainer");
-        TableView table = (TableView) scene.lookup("#ListofItems");
+        VBox gridContainer = (VBox) mainPage.lookup("#gridContainer");
+        TableView table = (TableView) mainPage.lookup("#ListofItems");
         if (table != null) {
             gridContainer.getChildren().remove(table);
         }
-        // if (grid != null) {
-        //     grid.getChildren().clear();
-        //     grid.getColumnConstraints().removeAll(grid.getColumnConstraints());
-        //     grid.getRowConstraints().removeAll(grid.getRowConstraints());
-        // }
+
     }
 
     public static void newTableList() {
         newItemList();
-        VBox tables = (VBox) scene.lookup("#ListofTables");
+        VBox tables = (VBox) mainPage.lookup("#ListofTables");
         tables.getChildren().clear();
+    }
+
+    public static void clearAddItemDialog(){
+        GridPane grid = (GridPane) addItemDialog.lookup("#GridOfValues");
+        if (grid != null) {
+            grid.getChildren().clear();
+            grid.getColumnConstraints().removeAll(grid.getColumnConstraints());
+            grid.getRowConstraints().removeAll(grid.getRowConstraints());
+        }
     }
 }

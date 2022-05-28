21eeb4fc614e54ed30f079f5fe550b83550757c9
JisusSestiveDoff <Rusikbudettut@gmail.com>
2022-05-28 11:45:45 +0300

    Create ItemDialogs.java

---
+++ src/main/java/com/dialogs/ItemDialogs.java
@@ -0,0 +1,19 @@
+package com.dialogs;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.App;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+public interface ItemDialogs {
+    
+    public static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public void display() throws ClassNotFoundException, SQLException, IOException;
+}

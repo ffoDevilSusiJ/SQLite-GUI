7930f384ae77a4b419a213370b81fa6bf737bdd7
JisusSestiveDoff <Rusikbudettut@gmail.com>
2022-06-05 15:18:23 +0300

    Create AddColumnCell.java

---
+++ src/main/java/com/AddColumnCell.java
@@ -0,0 +1,52 @@
+package com;
+
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class AddColumnCell extends TableCell<ObservableList<Object>, String>{
+    
+    
+    String startValue = null;
+
+
+    @Override
+    public void updateItem(String item, boolean empty) {
+        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent arg0) {
+                System.out.println("ff");
+                
+            }
+            
+        });
+        super.updateItem(item, empty);
+        if (empty) {
+            setText(null);
+            setGraphic(null);
+        }
+        // } else {
+        //     if (isEditing()) {
+        //         if (label != null) {
+        //             label.setText("");
+        //         }
+        //         setText(null);
+        //         setGraphic(label);
+        //     } else {
+        //         getStyleClass().add("oneCell");
+                
+        //         setText(getString());
+        //         setGraphic(label);
+        //     }
+        // }
+    }
+
+    private String getString() {
+        return getItem() == null ? "" : getItem().toString();
+    }
+}
